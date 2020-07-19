12e7a848918f2c1dc08731e78ee3697762020393
Matthew Lemon <lemon@matthewlemon.com>
2020-07-19 17:13:22 +0100

    started to build main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,40 @@
+/*
+datamaps-go is a simple tool to extract from and send data to spreadsheets.
+*/
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func createConfigDir() (string, error) {
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	// check if config folder exists
+	config_path := filepath.Join(dir, "datamaps-go")
+	if _, err := os.Stat(config_path); os.IsNotExist(err) {
+		log.Println("Config directory does not exist.")
+		log.Println("Creating config directory.")
+		if err := os.Mkdir(filepath.Join(dir, "datamaps-go"), 0700); err != nil {
+			return "", err
+		}
+	} else {
+		log.Println("Config directory found.")
+	}
+	return dir, nil
+}
+
+// Entry point
+
 func main() {
-	//reader.ReadDML("/home/lemon/Documents/datamaps/input/datamap.csv")
-	//reader.ReadXLSX("/home/lemon/Documents/datamaps/input/A417%20Air%20Balloon_Q1%20Apr%20-%20June%202019_Return.xlsm")
+	dir, err := createConfigDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
 }
