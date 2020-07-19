69854b70faa41e16ff1a2659ebc0624037b14b4f
Matthew Lemon <lemon@matthewlemon.com>
2020-07-19 20:20:16 +0100

    enhanced main

--- cmd/main.go
+++ cmd/main.go
@@ -4,27 +4,37 @@ datamaps-go is a simple tool to extract from and send data to spreadsheets.
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-func createConfigDir() (string, error) {
+func setUp() (string, error) {
 	dir, err := os.UserConfigDir()
 	if err != nil {
 		return "", err
 	}
 	// check if config folder exists
 	config_path := filepath.Join(dir, "datamaps-go")
+	db_path := filepath.Join(config_path, "datamaps.db")
 	if _, err := os.Stat(config_path); os.IsNotExist(err) {
 		log.Println("Config directory does not exist.")
-		log.Println("Creating config directory.")
+		log.Printf("Creating config directory %s\n", config_path)
 		if err := os.Mkdir(filepath.Join(dir, "datamaps-go"), 0700); err != nil {
 			return "", err
 		}
 	} else {
 		log.Println("Config directory found.")
+	}
+	if _, err := os.Stat(db_path); os.IsNotExist(err) {
+		log.Println("Database does not exist.")
+		_, err = os.Create(db_path)
+		if err != nil {
+			return "", err
+		}
+		log.Printf("Creating database file at %s\n", db_path)
+	} else {
+		log.Println("Database file found.")
 	}
 	return dir, nil
 }
@@ -32,9 +42,8 @@ func createConfigDir() (string, error) {
 // Entry point
 
 func main() {
-	dir, err := createConfigDir()
+	dir, err := setUp()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(dir)
 }
