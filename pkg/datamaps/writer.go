add51ef91b96e604c00b447f58d5df5fa638b90e
Matthew Lemon <lemon@matthewlemon.com>
2020-08-01 19:16:48 +0100

    fixed extraneous variable

--- pkg/datamaps/writer.go
+++ pkg/datamaps/writer.go
@@ -13,29 +13,6 @@ import (
 )
 
 func ExportMaster(opts *Options) error {
-	// A master represents a set of file data from the database. Actually, in terms of the database,
-	// it should represent a "return".
-	//
-	// The meat of the master is of the format:
-	//		Key	1 | Key_1_Value_for_FD_1 | Key_1_Value_for_FD_2 | Key_1_Value_for_FD_3 | ... etc
-	//		Key 2 | Key_2_Value_for_FD_1 | Key_2_Value_for_FD_2 | Key_2_Value_for_FD_3 | ... etc
-	//		Key 3 | Key_3_Value_for_FD_1 | Key_3_Value_for_FD_2 | Key_3_Value_for_FD_3 | ... etc
-	//		...
-	// Could be represented as a slice or a map[string][]string
-	// SQL statement:
-	//
-	// SELECT datamap_line.key, return_data.value, return_data.filename
-	//                                    FROM (((return_data
-	//                                    INNER JOIN datamap_line ON return_data.dml_id=datamap_line.id)
-	//                                    INNER JOIN datamap ON datamap_line.dm_id=datamap.id)
-	//                                    INNER JOIN return on return_data.ret_id=return.id)
-	//                                    WHERE datamap.name="Tonk 1" AND return.name="Hunkers";
-
-	// filename := filepath.Join(opts.MasterOutPutPath, "master.xlsx")
-
-	// a test key
-	targetKey := "A Rabbit"
-
 	wb := xlsx.NewFile()
 	sh, err := wb.AddSheet("Master Data")
 
@@ -77,11 +54,10 @@ func ExportMaster(opts *Options) error {
                                           INNER JOIN datamap_line ON return_data.dml_id=datamap_line.id)
                                           INNER JOIN datamap ON datamap_line.dm_id=datamap.id)
                                           INNER JOIN return on return_data.ret_id=return.id)
-                                          WHERE datamap.name=? AND return.name=? AND datamap_line.key=?
-                                          GROUP BY datamap_line.key;`
+                                          WHERE datamap.name=? AND return.name=?;`
 
 	var rowCount int64
-	rowCountRes := db.QueryRow(sqlCount, opts.DMName, opts.ReturnName, targetKey) // TODO: fix this
+	rowCountRes := db.QueryRow(sqlCount, opts.DMName, opts.ReturnName)
 	if err != nil {
 		return fmt.Errorf("cannot query for row count of return data - %v", err)
 	}
