4457cd61f10565c5fa1abafc5e763fb676c4293f
Matthew Lemon <lemon@matthewlemon.com>
2020-12-08 13:35:39 +0000

    fixed missing datamaps pkg

---
+++ datamaps/reader_test.go
@@ -0,0 +1,251 @@
+package datamaps
+
+import (
+	"os"
+	"testing"
+)
+
+func TestReadDML(t *testing.T) {
+	d, _ := ReadDML("testdata/datamap.csv")
+	cases := []struct {
+		idx int
+		val string
+	}{
+		{0, "Project/Programme Name"},
+		{1, "Department"},
+		{2, "Delivery Body"},
+	}
+
+	for _, c := range cases {
+		if got := d[c.idx].Key; got != c.val {
+			t.Errorf("The test expected %s, got %s.", c.val, d[c.idx].Key)
+		}
+	}
+}
+
+func TestNoFileReturnsError(t *testing.T) {
+	// this file does not exist
+	_, err := ReadDML("/home/bobbins.csv")
+	// if we get no error, something has gone wrong
+
+	if err == nil {
+		t.Errorf("Should have thrown error %s", err)
+	}
+}
+
+func TestBadDMLLine(t *testing.T) {
+	_, err := ReadDML("/home/lemon/code/python/bcompiler-engine/tests/resources/datamap_empty_cols.csv")
+
+	if err == nil {
+		t.Errorf("No error so test failed.")
+	}
+}
+
+func TestGetSheetsFromDM(t *testing.T) {
+	slice, _ := ReadDML("testdata/datamap.csv")
+	sheetNames := getSheetNames(slice)
+
+	if len(sheetNames) != 15 {
+		t.Errorf("The test expected 14 sheets in slice, got %d.",
+			len(sheetNames))
+	}
+}
+
+func TestReadXLSX(t *testing.T) {
+	d := ReadXLSX("testdata/test_template.xlsx")
+	cases := []struct {
+		sheet, cellref, val string
+	}{
+		{"Summary", "A2", "Date:"},
+		{"Summary", "IG10", "botticelli"},
+		{"Another Sheet", "F5", "4.2"},
+		{"Another Sheet", "J22", "18"},
+	}
+
+	for _, c := range cases {
+		got := d[c.sheet][c.cellref].Value
+		if got != c.val {
+			t.Errorf("The test expected %s in %s sheet to be %s "+
+				" - instead it is %s.", c.cellref, c.sheet, c.val, d[c.sheet][c.cellref].Value)
+		}
+	}
+}
+
+// func TestExtractWithDBDatamap(t *testing.T) {
+// 	// setup - we need the datamap in the test database
+// 	db, err := setupDB("./testdata/test.db")
+// defer func() {
+// 	db.Close()
+// 	os.Remove("./testdata/test.db")
+// }()
+
+// 	if err != nil {
+// 		t.Fatal("Expected to be able to set up the database.")
+// 	}
+
+// 	opts := Options{
+// 		DBPath: "./testdata/test.db",
+// 		DMName: "First Datamap",
+// 		DMPath: "./testdata/datamap.csv",
+// 	}
+
+// 	if err := DatamapToDB(&opts); err != nil {
+// 		t.Errorf("Unable to write datamap to database file because %v.", err)
+// 	}
+
+// 	d := extractDBDatamap("First Datamap", "testdata/test_template.xlsx")
+// }
+
+func TestDMLSliceFromDatabase(t *testing.T) {
+	// setup - we need the datamap in the test database
+	db, err := setupDB("./testdata/test.db")
+	defer func() {
+		db.Close()
+		os.Remove("./testdata/test.db")
+	}()
+
+	if err != nil {
+		t.Fatal("Expected to be able to set up the database.")
+	}
+
+	opts := Options{
+		DBPath: "./testdata/test.db",
+		DMName: "First Datamap",
+		DMPath: "./testdata/datamap.csv",
+	}
+
+	if err := DatamapToDB(&opts); err != nil {
+		t.Errorf("Unable to write datamap to database file because %v.", err)
+	}
+
+	cases := []struct {
+		index int
+		key   string
+	}{
+		{0, "Project/Programme Name"},
+		{1, "Department"},
+		{2, "Delivery Body"},
+		{3, "Stoogge value"},
+		{4, "DRRDD - IPA ID Number"},
+		{5, "Controls Project ID number"},
+		{6, "Jokey Entry"},
+		{7, "Parrots Name"},
+	}
+
+	data, _ := DatamapFromDB("First Datamap", db)
+
+	for _, c := range cases {
+		got := data[c.index].Key
+		if got != c.key {
+			t.Errorf("The test expected %s but got %s\n", c.key, data[c.index].Key)
+		}
+	}
+
+	if data[0].Key != "Project/Programme Name" {
+		t.Errorf("expected to see Project/Programme Name and got %q\n", data[0])
+	}
+}
+
+func TestExtractUsingDBDM(t *testing.T) {
+	// setup - we need the datamap in the test database
+	db, err := setupDB("./testdata/test.db")
+	defer func() {
+		db.Close()
+		os.Remove("./testdata/test.db")
+	}()
+
+	if err != nil {
+		t.Fatal("Expected to be able to set up the database.")
+	}
+
+	opts := Options{
+		DBPath: "./testdata/test.db",
+		DMName: "First Datamap",
+		DMPath: "./testdata/datamap_matches_test_template.csv",
+	}
+
+	if err := DatamapToDB(&opts); err != nil {
+		t.Errorf("Unable to write datamap to database file because %v.", err)
+	}
+
+	d, _ := ExtractDBDatamap("First Datamap", "testdata/test_template.xlsx", db)
+	cases := []struct {
+		sheet, cellref, val string
+	}{
+		{"Introduction", "C9", "Test Department"},
+		{"Introduction", "J9", "Greedy Parrots"},
+		{"Introduction", "A1", "10"},
+		{"Introduction", "C22", "VUNT"},
+	}
+
+	for _, c := range cases {
+		got := d[c.sheet][c.cellref].Value
+		if got != c.val {
+			t.Errorf("test_template.xlsx: The test expected %s in %s sheet to be %s "+
+				"- instead it is %s.", c.sheet, c.cellref, c.val,
+				d[c.sheet][c.cellref].Value)
+		}
+	}
+}
+
+func TestExtract(t *testing.T) {
+	d := extract("testdata/datamap.csv", "testdata/test_template.xlsx")
+	cases := []struct {
+		sheet, cellref, val string
+	}{
+		{"Introduction", "C9", "Test Department"},
+		{"Introduction", "J9", "Greedy Parrots"},
+		{"Introduction", "A1", "10"},
+		{"Introduction", "C22", "VUNT"},
+	}
+
+	for _, c := range cases {
+		got := d[c.sheet][c.cellref].Value
+		if got != c.val {
+			t.Errorf("The test expected %s in %s sheet to be %s "+
+				"- instead it is %s.", c.sheet, c.cellref, c.val,
+				d[c.sheet][c.cellref].Value)
+		}
+	}
+
+	if d["Another Sheet"]["E26"].Value != "Integer:" {
+		t.Errorf("Expected E26 in Another Sheet sheet to be Integer: - instead it is %s", d["Another Sheet"]["E26"].Value)
+	}
+}
+
+// func TestGetTargetFiles(t *testing.T) {
+// 	// This is not a suitable test for parameterisation, but doing it this way anyway.
+// 	type args struct {
+// 		path string
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		want    []string
+// 		wantErr bool
+// 	}{
+// 		{"Get files in testdata",
+// 			args{"./testdata/"},
+// 			[]string{
+// 				"testdata/test_template.xlsm",
+// 				"testdata/test_template.xlsx",
+// 				"testdata/test_template2.xlsx",
+// 				"testdata/test_template3.xlsx",
+// 			},
+// 			false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Log("Running the test")
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := getTargetFiles(tt.args.path)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("getTargetFiles() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("getTargetFiles() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
---
+++ datamaps/writer.go
@@ -0,0 +1,153 @@
+package datamaps
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/tealeg/xlsx/v3"
+
+	// Needed for the sqlite3 driver
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// CreateMaster creates a master spreadsheet for a specific return given,
+// based on a datamap name - both of which already need to be in the database,
+// along with the data associated with the return. The datamap and return data
+// must already have been imported.
+func CreateMaster(opts *Options) error {
+	wb := xlsx.NewFile()
+	sh, err := wb.AddSheet("Master Data")
+	if err != nil {
+		return fmt.Errorf("cannot add 'Master Data' sheet to new XLSX file: %v", err)
+	}
+
+	db, err := sql.Open("sqlite3", opts.DBPath)
+	if err != nil {
+		return fmt.Errorf("cannot open database %v", err)
+	}
+
+	// Get number amount of datamap keys in target datamap
+	keyCountRows := db.QueryRow("SELECT count(key) FROM datamap_line, datamap WHERE datamap.name=?;", opts.DMName)
+
+	var datamapKeysNumber int64
+	if err := keyCountRows.Scan(&datamapKeysNumber); err != nil {
+		return err
+	}
+
+	datamapKeysRows, err := db.Query("SELECT key FROM datamap_line, datamap WHERE datamap.name=?;", opts.DMName)
+	if err != nil {
+		return fmt.Errorf("cannot query for keys in database - %v", err)
+	}
+
+	var datamapKeys []string
+
+	var i int64
+	for i = 0; i < datamapKeysNumber; i++ {
+		if k := datamapKeysRows.Next(); k {
+			var key string
+			if err := datamapKeysRows.Scan(&key); err != nil {
+				return fmt.Errorf("cannot Scan for key %s - %v", key, err)
+			}
+			datamapKeys = append(datamapKeys, key)
+		}
+	}
+
+	sqlCount := `SELECT count(return_data.id)
+                                          FROM (((return_data
+                                          INNER JOIN datamap_line ON return_data.dml_id=datamap_line.id)
+                                          INNER JOIN datamap ON datamap_line.dm_id=datamap.id)
+                                          INNER JOIN return on return_data.ret_id=return.id)
+                                          WHERE datamap.name=? AND return.name=?;`
+
+	var rowCount int64
+	rowCountRes := db.QueryRow(sqlCount, opts.DMName, opts.ReturnName)
+
+	if err := rowCountRes.Scan(&rowCount); err != nil {
+		return err
+	}
+
+	getDataSQL := `SELECT datamap_line.key, return_data.vFormatted, return_data.filename
+                                          FROM (((return_data
+                                          INNER JOIN datamap_line ON return_data.dml_id=datamap_line.id) 
+                                          INNER JOIN datamap ON datamap_line.dm_id=datamap.id) 
+                                          INNER JOIN return on return_data.ret_id=return.id) 
+                                          WHERE datamap.name=? AND return.name=? AND datamap_line.key=?
+										  ORDER BY return_data.filename;`
+
+	seen := make(map[string]struct{}) // homemade Set https://emersion.fr/blog/2017/sets-in-go/
+
+	var values = make(map[string][]string)
+	headerSlice := make([]string, 0)
+	for _, k := range datamapKeys {
+		masterData, err := db.Query(getDataSQL, opts.DMName, opts.ReturnName, k)
+		if err != nil {
+			return err
+		}
+		for masterData.Next() {
+			var key, filename, fmttedValue string
+			if err := masterData.Scan(&key, &fmttedValue, &filename); err != nil {
+				fmt.Errorf("Problem scanning data from database for master: %v", err)
+			}
+			values, err = appendValueMap(key, fmttedValue, values)
+			if _, ok := seen[filename]; !ok {
+				headerSlice = append(headerSlice, filename)
+				seen[filename] = struct{}{}
+			}
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for masterRow := 0; masterRow <= len(datamapKeys); masterRow++ {
+		r, err := sh.AddRowAtIndex(masterRow)
+		if err != nil {
+			return fmt.Errorf("cannot create row %d in output spreadsheet: %v", masterRow, err)
+		}
+		if masterRow == 0 {
+			if hdr := r.WriteSlice(append([]string{""}, headerSlice...), -1); hdr == -1 {
+				return fmt.Errorf("cannot write header values into header row: %v", err)
+			}
+			continue
+		}
+		dmlKey := datamapKeys[masterRow-1]
+
+		// TODO - we need to format the cells here too, e.g. dates
+		if sl := r.WriteSlice(append([]string{dmlKey}, values[dmlKey]...), -1); sl == -1 {
+			log.Printf("not a slice type")
+		}
+	}
+
+	log.Printf("saving master at %s", opts.MasterOutPutPath)
+	if err := wb.Save(filepath.Join(opts.MasterOutPutPath, "master.xlsx")); err != nil {
+		log.Fatalf("cannot save file to %s", opts.MasterOutPutPath)
+	}
+	sh.Close()
+	return nil
+}
+
+func appendValueMap(k, v string, values map[string][]string) (map[string][]string, error) {
+
+	var keyIn bool
+	for kv := range values {
+		if kv == k {
+			keyIn = true
+			break
+		}
+	}
+
+	if keyIn {
+		slice, ok := values[k]
+		if !ok {
+			return nil, fmt.Errorf("%s is not a key in this map", k)
+		}
+		slice = append(slice, v)
+		values[k] = slice
+		return values, nil
+	} else {
+		values[k] = []string{v}
+		return values, nil
+	}
+}
